c781c0129d850877168a10913fa79e2e60dbe982
mauriciocortez73 <maumau123br.cortez@gmail.com>
2023-05-26 12:48:23 -0300

    Create locadora.sql
    Banco de dados

---
+++ locadora.sql
@@ -0,0 +1,152 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 26/05/2023 às 17:46
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `locadora`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `ator`
+--
+
+CREATE TABLE `ator` (
+  `id` int(11) NOT NULL,
+  `nome_artistico` varchar(35) NOT NULL,
+  `nome_real` varchar(50) NOT NULL,
+  `nascimento` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(35) NOT NULL,
+  `celular` varchar(15) NOT NULL,
+  `email` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `filme`
+--
+
+CREATE TABLE `filme` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(35) NOT NULL,
+  `ano` year(4) NOT NULL,
+  `valor` float NOT NULL,
+  `categoria` varchar(35) NOT NULL,
+  `ator` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `locacao`
+--
+
+CREATE TABLE `locacao` (
+  `retirada` date NOT NULL,
+  `devolucao` date NOT NULL,
+  `valor` float NOT NULL,
+  `cliente` int(11) NOT NULL,
+  `filme` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `ator`
+--
+ALTER TABLE `ator`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `filme`
+--
+ALTER TABLE `filme`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `filme_ator` (`ator`);
+
+--
+-- Índices de tabela `locacao`
+--
+ALTER TABLE `locacao`
+  ADD PRIMARY KEY (`retirada`),
+  ADD KEY `locacao_filme` (`filme`),
+  ADD KEY `locacao_cliente` (`cliente`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `ator`
+--
+ALTER TABLE `ator`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `filme`
+--
+ALTER TABLE `filme`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `filme`
+--
+ALTER TABLE `filme`
+  ADD CONSTRAINT `filme_ator` FOREIGN KEY (`ator`) REFERENCES `ator` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Restrições para tabelas `locacao`
+--
+ALTER TABLE `locacao`
+  ADD CONSTRAINT `locacao_cliente` FOREIGN KEY (`cliente`) REFERENCES `cliente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `locacao_filme` FOREIGN KEY (`filme`) REFERENCES `filme` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
