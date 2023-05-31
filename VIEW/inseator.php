05c720129035a8a447fc75cb083e46a36d29d833
felipe7272 <131923684+felipe-totvs@users.noreply.github.com>
2023-05-31 15:59:55 -0300

    2

---
+++ VIEW/inseator.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</head>
+<body>
+<div class="center grey">
+        <h1> Inserir Ator/h1>
+    </div>
+    <div class= 'suportedois'>
+
+
+   <div style="margin-top: 30px;" class="container">
+<form method="POST" action="">
+
+<h3>Inserir Autor</h3><br>
+<label class="autor">ID: </label><input type="number" name="ID" size="50">
+<br>
+<label class="autor">Nome Artistico:  </label><input type="text" name="nameart" size="47">
+<br>
+<label class="autor">Nome Real:  </label><input type="text" name="nomereal" size="47">
+<br>
+<br>
+<label class="autor">Data de Nascimento:  </label><input type="date" name="tData" id="cData">
+<br>
+<p><input type="submit" value="Confirmar" name="confirmar"><input type="reset" value="Limpar" name="B2"></p>
+</form>
+<br>
+</div>
+</div>
+</body>
+</html>
--- VIEW/insecliente.php
+++ VIEW/insecliente.php
@@ -16,5 +16,26 @@
     <div class="center grey">
         <h1> Inserir Cliente</h1>
     </div>
+    <div class= 'suportedois'>
+
+
+   <div style="margin-top: 30px;" class="container">
+<form method="POST" action="">
+
+<h3>Inserir Autor</h3><br>
+<label class="autor">ID: </label><input type="number" name="ID" size="50">
+<br>
+<label class="autor">Nome:  </label><input type="text" name="nameart" size="47">
+<br>
+<label class="autor">E-Mail:  </label><input type="text" name="email" size="47">
+<br>
+<label class="autor">Celular:  </label><input type="number" name="tel" size="47">
+<br>
+<br>
+<p><input type="submit" value="Confirmar" name="confirmar"><input type="reset" value="Limpar" name="B2"></p>
+</form>
+<br>
+</div>
+</div>
 </body>
 </html>
