be41d9b4704ca9ac95449e48019461a4158a0955
felipe-totvs <131923684+felipe-totvs@users.noreply.github.com>
2023-06-22 16:43:01 -0300

    update
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ DAL/dalUsuario.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace DAL;
+
+include_once 'C:\xampp\htdocs\lpbcct2php2023\DAL\conexao.php';
+include_once 'C:\xampp\htdocs\lpbcct2php2023\MODEL\Usuario.php';
+
+
+class dalUsuario
+{
+
+    public function SelectUser(string $usuario)
+    {
+        $sql = "select * from usuario where usuario LIKE ?;";
+        $pdo = Conexao::conectar();
+        $query = $pdo->prepare($sql);
+        $query->execute(array($usuario));
+        $linha = $query->fetch(\PDO::FETCH_ASSOC);
+        Conexao::desconectar();
+
+        $usuario = new \MODEL\Usuario();
+
+        if ($linha != null) {
+            $usuario->setId($linha['id']);
+            $usuario->setUsuario($linha['usuario']);
+            $usuario->setSenha($linha['senha']);
+            $usuario->setEmail($linha['email']);
+        }
+
+        return $usuario;
+    }
+}
