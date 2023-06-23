2f12cd02e1c8c4652b0626121620d43bd2de14d5
Maurício Cortez <maumau123br.cortez@gmail.com>
2023-06-23 11:19:10 -0300

    update

--- VIEW/footer.php
+++ VIEW/footer.php
@@ -1,7 +1,7 @@
 <footer class="page-footer  brown lighten-1">
     <div class="footer-copyright ajax  brown darken-1">
         <div class="container">
-            © 2023 MauMau fe/pa
+            © 2023 MauMau fe/pa  Tururu12K
             <a class="grey-text text-lighten-4 right" href="https://www.fema.edu.br">www.fema.edu.br</a>
         </div>
     </div>
